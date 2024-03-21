f26c66e90779eb4d598fe07b5a8ca85085f5fac4
Paras Jain <583119+parasjain01@users.noreply.github.com>
2024-03-20 20:29:56 -0400

    Adding the jar file

--- activemq-samples/jms-transactions/src/main/java/org/aws/samples/amazonmq/activemq/transactions/jms20/TransactionsTest.java
+++ activemq-samples/jms-transactions/src/main/java/org/aws/samples/amazonmq/activemq/transactions/jms20/TransactionsTest.java
@@ -1,30 +1,38 @@
+package org.aws.samples.amazonmq.activemq.transactions.jms20;
+
 import jakarta.jms.*;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 public class TransactionsTest {
     public static void main(String[] args) {
-        if(args.length < 4){
-            System.out.println("Provide arguments in required format");
+        if(args.length < 5 ){
+            System.out.println("Provide all the arguments in the required format");
+            System.out.println("java -jar <jar-file> brokerUsername brokerPassword brokerURL failTransaction orderId");
             System.exit(1);
         }
+
         String userName = args[0];
-        String password = args[0];
+        String password = args[1];
         String brokerURL = args[2];
         String failTransaction = args[3];
-        if(userName == null || password == null || brokerURL == null || failTransaction == null){
-            System.out.println("Provide arguments in required format");
+        String orderId = args[4];
+
+        if(isNullOrEmpty(userName) || isNullOrEmpty(password) ||
+                isNullOrEmpty(brokerURL) || isNullOrEmpty(failTransaction) ||
+                isNullOrEmpty(orderId)){
+            System.out.println("Provide valid arguments in required format");
+            System.out.println("java -jar <jar-file> brokerUsername brokerPassword brokerURL failTransaction orderId");
             System.exit(1);
         }
+
         ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerURL);
         try(JMSContext jmsContext = connectionFactory.createContext(userName, password, Session.SESSION_TRANSACTED)){
             JMSProducer producer = jmsContext.createProducer();
-
-
             Queue warehouseQueue = jmsContext.createQueue("warehouse-queue");
             Queue shippingQueue = jmsContext.createQueue("shipping-queue");
 
             //Send message to warehouse queue
-            producer.send(warehouseQueue, "PREPARE ORDERID 12345");
+            producer.send(warehouseQueue, "PREPARE ORDERID " + orderId);
             System.out.println("Message sent to Warehouse Queue");
 
             if(Boolean.valueOf(failTransaction)){
@@ -32,18 +40,16 @@ public class TransactionsTest {
             }
 
             //Send message to shipping queue
-            producer.send(shippingQueue, "SHIP ORDERID 12345");
+            producer.send(shippingQueue, "SHIP ORDERID " + orderId);
             System.out.println("Message sent to Shipping Queue");
             jmsContext.commit();
-
             System.out.println("Both the messages are sent");
-
         }catch (Exception e){
             e.printStackTrace();
-            System.out.println("Some exception occurred");
         }
+    }
 
-
-
+    private static boolean isNullOrEmpty(String str) {
+        return str == null || str.trim().isEmpty();
     }
 }
